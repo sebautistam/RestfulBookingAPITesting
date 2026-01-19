7966a60931eeb1cba46983900aa9c8422ba23540
Sergio E Bautista M <sebautistam@gmail.com>
2026-01-18 22:08:39 -0500

    Updated Search tests with Status Code

--- src/tests/search.test.js
+++ src/tests/search.test.js
@@ -30,67 +30,71 @@ describe('Seach for a Booking Flow', function(){
         bookingId= createResponse.body.bookingid;
     });
 
-    describe('Create a Booking and look for it',  function() {
-        
-        it('Find Booking time by First Name is under 3000ms', async function () {
-            const response = await got.get(`${BASE_URL}/booking`, {
-                searchParams: { firstname: env.first_name }
-            });
-            expect(response.timings.phases.total).to.be.below(3000);
-        });
+    describe('Search for a booking by First Name',  function() {
+        let response, responseBody;
 
-        it('Look for Booking by First Name', async function() {
-            const response = await got.get(`${BASE_URL}/booking`, {
+        before(async function() {
+            response = await got.get(`${BASE_URL}/booking`, {
                 searchParams: { firstname: env.first_name },
                 responseType: 'json'
             });
-            const responseBody = response.body;
-            
+            responseBody = response.body;
+        });
+        
+        it('Find Booking time by First Name is under 3000ms', async function () {
+            expect(response.timings.phases.total).to.be.below(3000);
+        });
+
+        it('Search Booking by First Name returns status 200', async function () {
+            expect(response.statusCode).to.equal(200);
+        });
+
+        it('Look for Booking by First Name', async function() {
             const found = responseBody.some(booking => booking.bookingid === bookingId);
             expect(found).to.be.true;
         });
 
         it('Search Booking by First Name response matches schema', async function () {
-            const response = await got.get(`${BASE_URL}/booking`, {
-                searchParams: { firstname: env.first_name },
-                responseType: 'json'
-            });
-
-            const { error } = bookingSearchResponseSchema.validate(response.body);
+            const { error } = bookingSearchResponseSchema.validate(responseBody);
             expect(error, error?.message).to.be.undefined;
         });
+    });
 
-        it('Find Booking time by Last Name is under 3000ms', async function () {
-            const response = await got.get(`${BASE_URL}/booking`, {
-                searchParams: { lastname: env.last_name }
-            });
-            expect(response.timings.phases.total).to.be.below(3000);
-        });
+    describe('Search for a booking by Last Name',  function() {
 
-        it('Look for Booking by Last Name', async function() {
-            const response = await got.get(`${BASE_URL}/booking`, {
+        let response, responseBody;
+
+        before( async function() {
+            response = await got.get(`${BASE_URL}/booking`, {
                 searchParams: { lastname: env.last_name },
                 responseType: 'json'
             });
-            const responseBody = response.body;
+            responseBody = response.body;
+        });
 
+        it('Find Booking time by Last Name is under 3000ms', async function () {
+            expect(response.timings.phases.total).to.be.below(3000);
+        });
+
+        it('Search Booking by Last Name returns status 200', async function () {
+            expect(response.statusCode).to.equal(200);
+        });
+
+        it('Look for Booking by Last Name', async function() {
             const found = responseBody.some(booking => booking.bookingid === bookingId);
             expect(found).to.be.true;
         });
 
         it('Search Booking by Last Name response matches schema', async function () {
-            const response = await got.get(`${BASE_URL}/booking`, {
-                searchParams: { lastname: env.last_name },
-                responseType: 'json'
-            });
-
-            const { error } = bookingSearchResponseSchema.validate(response.body);
+            const { error } = bookingSearchResponseSchema.validate(responseBody);
             expect(error, error?.message).to.be.undefined;
         });
         
     });
 
     describe('Update Only Name in Booking and look for it', function() {
+
+        let response, responseBody;
 
         before(async function(){
             const updateResponse = await got.patch(`${BASE_URL}/booking/${bookingId}`, {
@@ -99,37 +103,32 @@ describe('Seach for a Booking Flow', function(){
                     'Authorization': `Basic ${Buffer.from(`${env.admin_username}:${env.admin_password}`).toString('base64')}`
                 }
             }).json();
+
+            response = await got.get(`${BASE_URL}/booking`, {
+                searchParams: { firstname: env.first_name2 },
+                responseType: 'json'
+            });
+            responseBody = response.body;
         });
 
         it('Find Booking time by Updated First Name is under 3000ms', async function () {
-            const response = await got.get(`${BASE_URL}/booking`, {
-                searchParams: { firstname: env.first_name2 },
-                responseType: 'json'
-            });
             expect(response.timings.phases.total).to.be.below(3000);
         });
 
+        it('Search Booking by Updated First Name returns status 200', async function () {
+            expect(response.statusCode).to.equal(200);
+        });
+
         it('Look for Booking by Updated First Name', async function() {
-            const response = await got.get(`${BASE_URL}/booking`, {
-                searchParams: { firstname: env.first_name2 },
-                responseType: 'json'
-            });
-            const responseBody = response.body;
-            
             const found = responseBody.some(booking => booking.bookingid === bookingId);
             expect(found).to.be.true;
         });
 
         it('Search Booking by Updated First Name response matches schema', async function () {
-            const response = await got.get(`${BASE_URL}/booking`, {
-                searchParams: { firstname: env.first_name2 },
-                responseType: 'json'
-            });
-
-            const { error } = bookingSearchResponseSchema.validate(response.body);
+            const { error } = bookingSearchResponseSchema.validate(responseBody);
             expect(error, error?.message).to.be.undefined;
         });
-        
+
     });
 
 });
